e35911e4bdae5a7bfd938f2060e234c2f259adee
Eliezel <promiseseliezel@gmail.com>
2024-04-11 14:48:41 -0700

    languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const firstL = 'C is fun';
+const secondL = 'Python is cool';
+const thirdL = 'JavaScript is amazing';
+console.log(firstL);
+console.log(secondL);
+console.log(thirdL);
+
