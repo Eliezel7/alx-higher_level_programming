e4c3cf2f633b4123d96d304f819162b9f6494895
Eliezel <promiseseliezel@gmail.com>
2024-04-11 15:30:10 -0700

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,4 +1,3 @@
-
 #!/usr/bin/node
 const { argv } = require('process');
 const occurence = Number(argv[2]);
@@ -10,4 +9,3 @@ const display = () => {
 isNaN(occurence)
   ? (console.log('Missing number of occurrences'))
   : (display());
-
