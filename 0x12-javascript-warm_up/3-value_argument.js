8575d67a3605411c854f44ff09948730251a41d1
Eliezel <promiseseliezel@gmail.com>
2024-04-11 15:05:37 -0700

    Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const { arg } = require('process');
-if (arg[2] === undefined) { console.log('No argument'); } else { console.log(arg[2]); }
+const { argv } = require('process');
+if (argv[2] === undefined) { console.log('No argument'); } else { console.log(argv[2]); }
